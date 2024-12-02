2b6495ddb797a319e60b59e735ce60ee3ee4ec72
panzhongxian <panzhongxian0532@gmail.com>
2024-12-02 11:05:46 +0800

    Add empty byte array as redis nil result

--- cachecfg/rediscc.go
+++ cachecfg/rediscc.go
@@ -2,7 +2,7 @@ package cachecfg
 
 import (
 	"context"
-
+	"errors"
 	"github.com/go-redis/redis/v8"
 )
 
@@ -10,12 +10,21 @@ var _ ValueFetcher[[]byte] = &RedisKeyValueFetcher{}
 
 type RedisKeyValueFetcher struct {
 	Rds *redis.Client
+
+	// 配置
+	EmptyArrayAsNil bool
 }
 
 func (r *RedisKeyValueFetcher) FetchValue(args ...any) ([]byte, error) {
 	ctx := args[0].(context.Context)
 	key := r.Key(args...)
 	s, err := r.Rds.Get(ctx, key).Bytes()
+	/* If taking redis.Nil as a common blank value, you can return nil as error.
+	   This will not cause querying redis every time when calling `GetValue`.
+	*/
+	if r.EmptyArrayAsNil && errors.Is(err, redis.Nil) {
+		return make([]byte, 0), nil
+	}
 	if err != nil {
 		return nil, err
 	}
